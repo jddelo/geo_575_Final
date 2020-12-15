c524577d29e4ba00cc6da2d355616793227b3369
dmenaker <58150573+dmenaker@users.noreply.github.com>
2020-12-14 23:06:54 -0500

    update

--- js/main.js
+++ js/main.js
@@ -59,73 +59,6 @@ function iconByAcres(feature){
 var imagery = L.esri.basemapLayer('ImageryFirefly'),
     topo = L.esri.basemapLayer('Topographic'),
 
-    fires = L.esri.Heat.featureLayer({
-    //url: 'https://sampleserver6.arcgisonline.com/arcgis/rest/services/CommunityAddressing/MapServer/0',
-    url: 'https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/mtbs_FODpoints_DD_wgs84/FeatureServer/0',
-    radius: 60});
-
-    gray = L.esri.basemapLayer('DarkGray'),
-    
-    places = L.esri.tiledMapLayer({
-        url: 'https://services.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer'}),
-    
-    //Add the fire data, style it based on acres burned, and symbolize with custom icon
-    fires = L.esri.featureLayer({
-        url: 'https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/mtbs_FODpoints_DD_wgs84/FeatureServer/0',
-        where: "datefmt = '2000-01'",
-        pointToLayer: function(feature, latlng) {return L.marker(latlng, {icon: iconByAcres(feature)})},
-    }),
-    
-    
-    //Add and symbolize the drought data
-    drought = L.esri.featureLayer({
-        url: 'https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/Drought_Data_2000_2019/FeatureServer/0',
-       //simplifyFactor: 0.35,
-        useCors: true,
-        where: "datefmt = '2000-01'",
-
-        style: function (feature){
-            if (feature.properties.DM === 4) {
-                return {color: '#730000', fillOpacity: '0.7', opacity: '0.5', weight: 1};
-            } else if (feature.properties.DM === 3) {
-                return {color: '#e60000',  fillOpacity: '0.65',opacity: '0.5', weight: 1};
-            } else if (feature.properties.DM === 2) {
-                return {color: '#fa0',  fillOpacity: '0.65',opacity: '0.5', weight: 1};
-            } else if (feature.properties.DM === 1) {
-                return {color: '#fcd37f',  fillOpacity: '0.65', opacity: '0.5', weight: 1};
-            } else {
-                return {color: '#ff0',  fillOpacity: '0.5',opacity: '0.5', weight: 1};
-            }
-        }
-    });
-    
-    
-    //Add state data
-    states = L.esri.featureLayer({
-    url: 'https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/States_drought/FeatureServer/0',
-    useCors: true,
-    simplifyFactor: 0.6,
-    precision: 4,
-    onEachFeature: onEachFeature,
-    pane: 'statespane',
-    style: {fillOpacity: 0, weight:1.5, color:'#8c8c8c'}
-    });
-
-
-    //Create pop up for fires
-    fires.bindPopup(function (layer){
-        return L.Util.template('<p>{BurnBndAc} Acres Burned in {datefmt}</p>', layer.feature.properties);
-    });
-    fires.on('mouseover', function () {
-        this.openPopup();
-    });
-    fires.on('mouseout', function () {
-        this.closePopup();
-    });
-
-
-
-
     gray = L.esri.basemapLayer('DarkGray'),
     
     places = L.esri.tiledMapLayer({
@@ -172,7 +105,6 @@ var imagery = L.esri.basemapLayer('ImageryFirefly'),
     pane: 'statespane',
     style: {fillOpacity: 0, weight:1.5, color:'#8c8c8c'}
     });
-
 
     //Create pop up for fires
     fires.bindPopup(function (layer){
@@ -218,34 +150,30 @@ function createMap(){
     //run the create sequence control function
     createSequenceControls(mymap);
     
+    //create the legend
     newLegend(mymap);
 
     
     //add layers & layer control to map
     L.control.layers(baseMaps, overlayMaps).addTo(mymap);
 
-
-    //run the create sequence control function
-    createSequenceControls(mymap);
-
-
 //define update proportional symbols function 
-function updatePropSymbols(mymap,attribute){
-    mymap.eachLayer(function(layer){
-        if (layer.feature && layer.feature.properties[attribute]){
-            var props = layer.feature.properties;
-            //calculate radius based on value from currently selected year
-            var radius = calcPropRadius(props[attribute]);
-            layer.setRadius(radius);
-            //create new popups based on new values
-            var popup = new Popup(props, attribute, layer, radius);
-            popup.bindToLayer();
-            
-            //run update legend function after symbols are updated
-            updateLegend(mymap, attribute);
-        };
-    });
-};
+//function updatePropSymbols(mymap,attribute){
+//    mymap.eachLayer(function(layer){
+//        if (layer.feature && layer.feature.properties[attribute]){
+//            var props = layer.feature.properties;
+//            //calculate radius based on value from currently selected year
+//            var radius = calcPropRadius(props[attribute]);
+//            layer.setRadius(radius);
+//            //create new popups based on new values
+//            var popup = new Popup(props, attribute, layer, radius);
+//            popup.bindToLayer();
+//            
+//            //run update legend function after symbols are updated
+//            updateLegend(mymap, attribute);
+//        };
+//    });
+//};
 
 
     //add event listener for year query change and call updateYear function
@@ -422,60 +350,60 @@ function onEachFeature(feature, states) {
     });
 };
 
-//function to change the month being displayed
-function updateMonth(rangeindex) {
-    var curDate = document.getElementById('dateshown').innerHTML;
-    if (rangeindex < 10){
-        var newDate = curDate.slice(0,4) + "-0" + rangeindex.toString();
-    } else {
-        var newDate = curDate.slice(0,4) + "-" + rangeindex.toString();
-    }
-    var newQuery = "datefmt = '" + newDate + "'";
-    console.log("newQuery- " + newQuery)
-    drought.setWhere(newQuery);
-    fires.setWhere(newQuery);
-    $('#dateshown').html(newDate);
-    
-};
-
-//create function to change the year being displayed
-function updateYear() {
-    var curDate = document.getElementById('dateshown').innerHTML;
-        dateParts = curDate.split("-", 2);
-        newYear = document.getElementById('yearselect').value;
-        newDate = newYear + "-" + dateParts[1];
-        newQuery = "datefmt = '" + newDate + "'";
-        drought.setWhere(newQuery);
-        fires.setWhere(newQuery);
-        $('#dateshown').html(newDate);    
-};
-
-function highlightState(e) {
-    var layer = e.target;
-
-    layer.setStyle({
-        weight: 4,
-        color: '#000000',
-        fillOpacity: 0
-    });
-};
-
-function unhighlightState(e) {
-    states.resetStyle(e.target);
-    //states.setStyle({
-      //  fillColor: 'none', 
-        //weight:1.5, 
-        //color:'#8c8c8c'
-    //});
-};
-
-function onEachFeature(feature, states) {
-    states.on({
-        mouseover: highlightState,
-        mouseout: unhighlightState
-
-    });
-};
+////function to change the month being displayed
+//function updateMonth(rangeindex) {
+//    var curDate = document.getElementById('dateshown').innerHTML;
+//    if (rangeindex < 10){
+//        var newDate = curDate.slice(0,4) + "-0" + rangeindex.toString();
+//    } else {
+//        var newDate = curDate.slice(0,4) + "-" + rangeindex.toString();
+//    }
+//    var newQuery = "datefmt = '" + newDate + "'";
+//    console.log("newQuery- " + newQuery)
+//    drought.setWhere(newQuery);
+//    fires.setWhere(newQuery);
+//    $('#dateshown').html(newDate);
+//    
+//};
+//
+////create function to change the year being displayed
+//function updateYear() {
+//    var curDate = document.getElementById('dateshown').innerHTML;
+//        dateParts = curDate.split("-", 2);
+//        newYear = document.getElementById('yearselect').value;
+//        newDate = newYear + "-" + dateParts[1];
+//        newQuery = "datefmt = '" + newDate + "'";
+//        drought.setWhere(newQuery);
+//        fires.setWhere(newQuery);
+//        $('#dateshown').html(newDate);    
+//};
+//
+//function highlightState(e) {
+//    var layer = e.target;
+//
+//    layer.setStyle({
+//        weight: 4,
+//        color: '#000000',
+//        fillOpacity: 0
+//    });
+//};
+//
+//function unhighlightState(e) {
+//    states.resetStyle(e.target);
+//    //states.setStyle({
+//      //  fillColor: 'none', 
+//        //weight:1.5, 
+//        //color:'#8c8c8c'
+//    //});
+//};
+//
+//function onEachFeature(feature, states) {
+//    states.on({
+//        mouseover: highlightState,
+//        mouseout: unhighlightState
+//
+//    });
+//};
 
 //create point layer from geojson layer
 //function pointToLayer(feature, latlng, attributes){
